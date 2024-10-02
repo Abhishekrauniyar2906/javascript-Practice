62b4c496b416c00b04a8c7569026737bd1c5c865
ABHISHEK RAUNIYAR <abhishekrauniyar55@gmail.com>
2024-10-02 16:02:04 +0530

    datatypes

---
+++ 01_Basic/02dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all Js code as a new version
+
+// alert(3+3) // we are using nodejs not browser
+
+console.log(3+3);
+ console.log("hello welocome into javascript lecture 2");
+
+// Data types
+
+// Primitive data Types
+
+// string
+// null
+// number
+// boolean
+//undefined
+// symbol
+
+// object
+
+let age = 18;
+let name = "abhishek"
+let isloggedIn = false;
+
+
+console.log(typeof "abhishek")
+console.log(typeof null)
+console.log(typeof 18)
+console.log(typeof isloggedIn)
