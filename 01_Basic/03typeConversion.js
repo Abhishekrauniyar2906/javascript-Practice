66627cf96d9eaf9d68c7d5061cae6d73310d8a58
ABHISHEK RAUNIYAR <abhishekrauniyar55@gmail.com>
2024-10-03 14:16:20 +0530

    03typeConversion

---
+++ 01_Basic/03typeConversion.js
@@ -0,0 +1,24 @@
+// let string = "33"
+
+// let check = Number(string)
+// console.log(string)
+// console.log(typeof Number)
+
+// let isloggedIn = false
+// let a = Boolean(isloggedIn)
+// console.log(a)
+// console.log(typeof isloggedIn)
+
+// let somenumber = 33
+
+// let stringnumber = String(somenumber)
+// console.log(stringnumber)
+// console.log(typeof stringnumber)
+
+let b = "@"
+
+let d = Boolean(b)
+
+console.log(b)
+console.log(d)
+console.log(typeof b)
